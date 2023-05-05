64843b1ac9bc2826aae12c008e9cdd0f0d4adee8
HS-YuChen <syc21706091056@163.com>
2023-05-05 10:47:18 +0800

    2023/5/5

--- jettyServer/src/main/java/com/yuchen/servlet/YuchenSerlet.java
+++ jettyServer/src/main/java/com/yuchen/servlet/YuchenSerlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 public class YuchenSerlet extends HttpServlet {
     private static final long serialVersionUID = 1930070168719207625L;
@@ -17,18 +19,24 @@ public class YuchenSerlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("经过get请求了");
-        StringBuilder sb = new StringBuilder();
         BufferedReader reader = req.getReader();
+        StringBuilder sb = new StringBuilder();
         String data = "";
         if((data = reader.readLine())!=null){
             sb.append(data);
         }
-        System.out.println(sb.toString());
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Content-Type","application/json");
         resp.getWriter().write(sb.toString());
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        String name = getParam("name", parameterMap);
+        String height = getParam("height", parameterMap);
+        String weight = getParam("weight", parameterMap);
+
         System.out.println("经过post请求了");
         StringBuilder sb = new StringBuilder();
         BufferedReader reader = req.getReader();
@@ -36,8 +44,21 @@ public class YuchenSerlet extends HttpServlet {
         if((data = reader.readLine())!=null){
             sb.append(data);
         }
+        String param = name+height+weight;
+        System.out.println(param);
         System.out.println(sb.toString());
-        resp.getWriter().write(sb.toString());
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Content-Type","application/json");
+        resp.getWriter().write(sb.toString()+param);
 
+    }
+
+
+    public String getParam(String str,Map<String,String[]> parameterMap){
+        String result = "";
+        if(parameterMap.containsKey(str)){
+            result = parameterMap.get(str)[0];
+        }
+        return result;
     }
 }
--- test/src/main/java/com/yuchen/demo/HttpClientDemo.java
+++ test/src/main/java/com/yuchen/demo/HttpClientDemo.java
@@ -4,43 +4,64 @@ package com.yuchen.demo;/*
 */
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.rowset.serial.SerialStruct;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 @RequestMapping("httpClient")
 @RestController
+@Slf4j
 public class HttpClientDemo {
 
+    @Value("${jettyServer}")
+    private String jettyServerUrl;
     @GetMapping("/testControllerYuchen")
-    public String testControllerYuchen() throws IOException, ParseException {
+    public String testControllerYuchen(){
+        String result = "";
+        try {
+//            CloseableHttpResponse closeableHttpResponse = postMethod(jettyServerUrl, "UTF-8", "宋雨晨");
+            CloseableHttpResponse closeableHttpResponse = getMethod(jettyServerUrl, "UTF-8", "宋雨晨");
+            //获取响应码
+            int code = closeableHttpResponse.getCode();
+            log.info("响应码为"+code);
+            //获取响应体
+            result = EntityUtils.toString(closeableHttpResponse.getEntity());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return result;
+    }
 
+    public CloseableHttpResponse postMethod(String url,String charset,String body) throws IOException {
+        //获取CloseableHttpClient对象
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        String url = "http://172.16.32.99:8088";
-        url += "/controller/yuchen";
-
+        //新建HttpPost对象
         HttpPost httpPost = new HttpPost(url);
-
-        String body = "雨晨调用第三方服务了，雨晨调用第三方服务了雨晨调用第三方服务了雨晨调用第三方服务了雨晨调用第三方服务了雨晨调用第三方服务了雨晨调用第三方服务了";
-        StringEntity stringEntity = new StringEntity(body);
-
+        StringEntity stringEntity = new StringEntity(body, Charset.forName(charset));
         httpPost.setEntity(stringEntity);
+        //发送请求 得到CloseableHttpResponse对象
+        return httpClient.execute(httpPost);
+    }
 
-        CloseableHttpResponse response = httpClient.execute(httpPost);
-
-        String s = EntityUtils.toString(response.getEntity());
-
-        return s;
-
+    public CloseableHttpResponse getMethod(String url,String charset,String body) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        StringEntity stringEntity = new StringEntity(body, Charset.forName(charset));
+        httpGet.setEntity(stringEntity);
+        return httpClient.execute(httpGet);
     }
 }
